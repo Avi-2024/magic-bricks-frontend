d45e61b63e35598b789511150823b8e94f0a0152
Avi Tamrakar <abis39112@gmail.com>
2025-06-07 08:59:52 +0530

    Fix import path for PropertyList.css

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./component/home/PropertyList.css";
+import "./component/home/propertyList.css";
 import "./profile.css";
 import { useNavigate } from "react-router-dom";
 
